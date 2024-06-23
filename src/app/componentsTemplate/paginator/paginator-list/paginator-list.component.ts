cddabd85ec3d5a0062adf0f0472f8e7b8c729a31
lucasemanoel385 <lucas.emanoelbastossilva@gmail.com>
2024-06-23 05:05:26 -0300

    add @angular/material

--- src/app/componentsTemplate/paginator/paginator-list/paginator-list.component.ts
+++ src/app/componentsTemplate/paginator/paginator-list/paginator-list.component.ts
@@ -1,4 +1,4 @@
-/*import { MatPaginatorIntl } from '@angular/material/paginator';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
@@ -22,4 +22,4 @@ export class PaginatorListComponent implements MatPaginatorIntl {
   }
 
 
-}*/
+}
