e611cdc23c4e9554b9337ed85f631e441463da03
lucasemanoel385 <lucas.emanoelbastossilva@gmail.com>
2024-09-30 19:37:15 -0300

    add withHashLocation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withRouterConfig } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideNgxMask } from 'ngx-mask';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { tokenJWTInterceptor } from './config/interceptor/token-jwt.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatNativeDateModule } from '@angular/material/core';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { tokenJWTInterceptor } from './config/interceptor-reqJwtToken/token-jwt.interceptor';
 //import { WebsocketService } from './config/webSocket/webSocket.service';
 
 registerLocaleData(localePt);
@@ -17,6 +17,9 @@ registerLocaleData(localePt);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding(), 
+
+    //Reload in Xampp
+    withHashLocation(),
     
     // Isso faz com que as rotas filhas sempre tenham acesso a parametros definidos da rota pai
     withRouterConfig(
